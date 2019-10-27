16ade990e07f2524fbeed636ea2f84844c9184d6
bahilo <sisi.bahilo@gmail.com>
2019-10-27 09:12:26 +0100

    ignore file update

---
+++ admin/config.php
@@ -0,0 +1,52 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mediamarket3/admin/');
+define('HTTP_CATALOG', 'http://localhost/mediamarket3/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mediamarket3/admin/');
+define('HTTPS_CATALOG', 'http://localhost/mediamarket3/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\wamp2\www\mediamarket3/admin/');
+define('DIR_SYSTEM', 'D:\wamp2\www\mediamarket3/system/');
+define('DIR_LANGUAGE', 'D:\wamp2\www\mediamarket3/admin/language/');
+define('DIR_TEMPLATE', 'D:\wamp2\www\mediamarket3/admin/view/template/');
+define('DIR_CONFIG', 'D:\wamp2\www\mediamarket3/system/config/');
+define('DIR_IMAGE', 'D:\wamp2\www\mediamarket3/image/');
+define('DIR_STORAGE', 'D:/wamp2/www/mediamarket3/storage/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+define('DIR_CATALOG', 'D:\wamp2\www\mediamarket3/catalog/');
+
+
+// define('DIR_APPLICATION', 'D:\wamp2\www\mediamarket3/catalog/');
+// define('DIR_SYSTEM', 'D:\wamp2\www\mediamarket3/system/');
+// define('DIR_LANGUAGE', 'D:\wamp2\www\mediamarket3/catalog/language/');
+// define('DIR_TEMPLATE', 'D:\wamp2\www\mediamarket3/catalog/view/theme/');
+// define('DIR_CONFIG', 'D:\wamp2\www\mediamarket3/system/config/');
+// define('DIR_IMAGE', 'D:\wamp2\www\mediamarket3/image/');
+//define('DIR_STORAGE', 'D:/wamp2/www/mediamarket3/storage/');
+// define('DIR_CACHE', DIR_STORAGE . 'cache/');
+// define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+// define('DIR_LOGS', DIR_STORAGE . 'logs/');
+// define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+// define('DIR_SESSION', DIR_STORAGE . 'session/');
+// define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'mediamarket');
+define('DB_PASSWORD', 'qobd225!');
+define('DB_DATABASE', 'mediamarket3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+
+define('OPENCART_SERVER', 'http://www.opencart.com/');
